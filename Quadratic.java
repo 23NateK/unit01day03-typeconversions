5fccb697ff6cf00406d07fb1c083e00556d0f737
23NateK <n.kimber22@gmail.com>
2023-01-11 11:49:18 -0600

    fixed Quad

--- Quadratic.java
+++ Quadratic.java
@@ -4,8 +4,8 @@ public class Quadratic {
         double b = Double.parseDouble(args[1]);
         double c = Double.parseDouble(args[2]);
 
-        System.out.println((-b+Math.sqrt(b*b-4*a*c)/2*a));
-        System.out.println((-b-Math.sqrt(b*b-4*a*c)/2*a));
+        System.out.println(((b*-1)+Math.sqrt(b*b-4*a*c)/2*a));
+        System.out.println(((-b-Math.sqrt((b*b)-(4*a*c)))/(2*a)));
 
 
 
