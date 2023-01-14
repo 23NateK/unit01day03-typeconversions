aee6e2aa679f08c5de90f5d366a04dcde57a4271
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-14 07:49:02 -0600

    added feedback

--- DoubleOps.java
+++ DoubleOps.java
@@ -1,15 +1,15 @@
 class DoubleOps {
     public static void main(String[] args) {
 
-
+        // TODO: rewrite these so when run, each number is labeled with the mathematical
+        // operation
         System.out.println(Math.abs(Integer.parseInt(args[0])));
 
         System.out.println(Math.abs(Double.parseDouble(args[1])));
 
-        System.out.println(Math.pow(Double.parseDouble(args[2]),Double.parseDouble(args[3])));
-        
-        System.out.println(Math.sqrt(Double.parseDouble(args[4])));
+        System.out.println(Math.pow(Double.parseDouble(args[2]), Double.parseDouble(args[3])));
 
+        System.out.println(Math.sqrt(Double.parseDouble(args[4])));
 
         System.out.println(Math.random());
 
